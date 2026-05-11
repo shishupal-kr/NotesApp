94e347ab3e5438a83aaebdee5e2918a4e580e67f
shishupal <shishupalking@gmail.com>
2026-05-11 23:50:34 +0530

    import maps

--- src/main/java/com/notes/notesapp/controller/NoteController.java
+++ src/main/java/com/notes/notesapp/controller/NoteController.java
@@ -3,6 +3,9 @@ package com.notes.notesapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class NoteController {
 
