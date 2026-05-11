5d97775fb169f6d31c3e3dd7348e62efba3a23db
shishupal <shishupalking@gmail.com>
2026-05-11 23:41:51 +0530

    get hello fix

--- src/main/java/com/notes/notesapp/controller/NoteController.java
+++ src/main/java/com/notes/notesapp/controller/NoteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoteController {
 
     @GetMapping("/hello")
-    public Map<String, String> hello() {
+    public Map<String, String> gethello() {
         Map<String, String> response = new HashMap<>();
         response.put("message", "hello how are you !");
         return response;
