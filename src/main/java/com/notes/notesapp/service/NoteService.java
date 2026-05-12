38244c718ab5e61f66ed7dcc197ecf17c627ee7c
shishupal <shishupalking@gmail.com>
2026-05-12 23:06:46 +0530

    added service layer code seperation

---
+++ src/main/java/com/notes/notesapp/service/NoteService.java
@@ -0,0 +1,21 @@
+package com.notes.notesapp.service;
+
+import com.notes.notesapp.dto.NoteDto;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class NoteService {
+    public Map<String, String> createNote(NoteDto noteDto) {
+
+        Map<String, String> response = new HashMap<>();
+
+        response.put("title", noteDto.getTitle());
+        response.put("content", noteDto.getContent());
+
+        return response;
+
+    }
+}
