f135193e2ee062a5193996fceef88859aab4601a
shishupal <shishupalking@gmail.com>
2026-05-12 21:08:25 +0530

    import postmapping

--- src/main/java/com/notes/notesapp/controller/NoteController.java
+++ src/main/java/com/notes/notesapp/controller/NoteController.java
@@ -1,6 +1,7 @@
 package com.notes.notesapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
