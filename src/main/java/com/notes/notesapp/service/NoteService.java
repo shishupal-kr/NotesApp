ffc7b4d11e0e6f8b539b8bad065e83e8b6063d1a
shishupal <shishupalking@gmail.com>
2026-05-14 21:49:52 +0530

    remove unused import

--- src/main/java/com/notes/notesapp/service/NoteService.java
+++ src/main/java/com/notes/notesapp/service/NoteService.java
@@ -7,9 +7,7 @@ import com.notes.notesapp.repository.NoteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class NoteService {
