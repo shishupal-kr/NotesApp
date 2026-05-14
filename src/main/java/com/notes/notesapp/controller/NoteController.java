8a9a084d95c9fee2a8d93492ad8a36796beb3d47
shishupal <shishupalking@gmail.com>
2026-05-14 21:48:43 +0530

    refine code

--- src/main/java/com/notes/notesapp/controller/NoteController.java
+++ src/main/java/com/notes/notesapp/controller/NoteController.java
@@ -1,16 +1,13 @@
 package com.notes.notesapp.controller;
 
 import com.notes.notesapp.dto.NoteResponseDto;
-import com.notes.notesapp.entity.Note;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.notes.notesapp.service.NoteService;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.notes.notesapp.dto.NoteRequestDto;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,16 +16,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class NoteController {
 
     private final NoteService noteService;  // using service layer
+
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
     }
 
     @PostMapping("/notes")
     public NoteResponseDto createNote(
-            @Valid @RequestBody NoteRequestDto noteDto // json to java object and store in NoteDto (deserialization)
+            @Valid @RequestBody NoteRequestDto noteRequestDto // json to java object and store in NoteDto (deserialization)
     ) {
         //logic in service layer
-        return noteService.createNote(noteDto);
+        return noteService.createNote(noteRequestDto);
     }
 
     @GetMapping("/notes")
