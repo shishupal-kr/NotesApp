6c5e2e7bfb3e7be88ba82ad457d508b2fc2c5081
shishupal <shishupalking@gmail.com>
2026-06-03 12:03:15 +0530

    refined

--- src/main/java/com/notes/notesapp/repository/NoteRepository.java
+++ src/main/java/com/notes/notesapp/repository/NoteRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface NoteRepository extends JpaRepository<Note, Long> {
-    List<Note> findByTitleContainingIgnoreCase(String title);
+    public interface NoteRepository extends JpaRepository<Note, Long> {
+        List<Note> findByTitleContainingIgnoreCase(String title);
 }
